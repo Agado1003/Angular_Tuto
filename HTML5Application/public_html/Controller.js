b67acf3af0a667885a6d7cadf46ec2c06add4ede
Agado1003 <adrien.gadeau@gmail.com>
2015-03-19 11:44:11 +0100

    For Each with 'ORDER BY' filter Signed-off-by: Agado1003 <adrien.gadeau@gmail.com>

--- HTML5Application/public_html/Controller.js
+++ HTML5Application/public_html/Controller.js
@@ -3,8 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-angular.module('myApp', []).controller('productCtrl', function($scope) {
-    $scope.Product = {Name:"Potato",Price:5.99};
+angular.module('myApp', []).controller('myCtrl', function($scope) {
+    $scope.Countries=[{Name:'France',Population:65000000},
+                      {Name:'Belgium',Population:12000000},
+                      {Name:'Germany',Population:80000000},
+                      {Name:'Finland',Population:5000000}];
 });
 
 
