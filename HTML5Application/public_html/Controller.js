3e7cc97fc8161b399514de806910753977a92d33
Agado1003 <adrien.gadeau@gmail.com>
2015-03-24 10:58:00 +0100

    Split VIEW, MODULE and CONTROLLER in separated files. Signed-off-by: Agado1003 <adrien.gadeau@gmail.com>

--- HTML5Application/public_html/Controller.js
+++ HTML5Application/public_html/Controller.js
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-angular.module('myApp', []).controller('myCtrl', function($scope) {
-    $scope.Countries=[{Name:'France',Population:65000000},
-                      {Name:'Belgium',Population:12000000},
-                      {Name:'Germany',Population:80000000},
-                      {Name:'Finland',Population:5000000}];
+app.controller('formCtrl', function($scope) {
+    $scope.master = {firstName: "John", lastName: "Doe"};
+    $scope.reset = function() {
+        $scope.user = angular.copy($scope.master);
+    };
+    $scope.reset();
 });
 
 
